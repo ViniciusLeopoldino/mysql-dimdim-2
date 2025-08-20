8cebec4fc64719ecf12fd104b5dbfeff99006d68
Vinicius Leopoldino <135030968+ViniciusLeopoldino@users.noreply.github.com>
2025-08-20 19:59:03 -0300

    Update init.sql

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -10,4 +10,4 @@ INSERT INTO transacoes (descricao, valor, data_transacao) VALUES
 ('Transferência PIX', -250.00, '2025-06-03 14:30:00'),
 ('Pagamento de conta', -180.50, '2025-06-05 09:15:00'),
 ('Saque em caixa eletrônico', -300.00, '2025-06-07 16:45:00'),
-('Investimento em poupança', -500.00, '2025-06-10 11:20:00');
+('Investimento em poupança test', -500.00, '2025-06-10 11:20:00');
